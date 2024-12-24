e021e62c76146bda8a5fc534495545f539eaf7ed
david amick <github@davidamick.com>
2024-12-23 17:02:52 -0800

    Refactor a little

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,39 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "bash-language-server",
-        "black",
-        "buf",
-        "css-lsp",
-        "deno",
-        "dockerfile-language-server",
-        "gofumpt",
-        "goimports-reviser",
-        "gopls",
-        "html-lsp",
-        "lua-language-server",
-        "prettier",
-        "protols",
-        "python-lsp-server",
-        "rust-analyzer",
-        "shellcheck",
-        "shfmt",
-        "stylua",
-        "yaml-language-server",
-        "yamlfmt",
-      },
-    },
-  },
-
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- enable format on save
-    opts = require("configs.conform"),
-  },
-
-  {
     "NvChad/nvterm",
     enabled = false,
   },
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "black",
+        "buf",
+        "css-lsp",
+        "deno",
+        "dockerfile-language-server",
+        "gofumpt",
+        "goimports-reviser",
+        "gopls",
+        "html-lsp",
+        "lua-language-server",
+        "prettier",
+        "protols",
+        "python-lsp-server",
+        "rust-analyzer",
+        "shellcheck",
+        "shfmt",
+        "stylua",
+        "yaml-language-server",
+        "yamlfmt",
+      },
+    },
+  },
+}
