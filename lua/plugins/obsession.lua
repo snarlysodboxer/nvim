daa1cb96a52bd7a512761154f399112c0a45d8f1
david amick <david@davidamick.com>
2025-12-29 10:31:17 -0800

    Add obsession

---
+++ lua/plugins/obsession.lua
@@ -0,0 +1,10 @@
+-- vim-obsession for tmux-resurrect session restoration
+-- Use :Obsession to start tracking, :Obsession! to stop
+-- Sessions are saved to Session.vim in current directory
+
+return {
+  {
+    "tpope/vim-obsession",
+    lazy = false,
+  },
+}
